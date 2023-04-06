fa962b8013739ad7f3e06b6fe709aec6ab0b02c5
Faille Axel <axelfaille45@gmail.com>
2023-04-06 15:28:44 +0200

    create view FAffichePraticien

---
+++ ProjetGsbEntity/Presentation/FAffichePraticien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetGsbEntity.Presentation
+{
+    public partial class FAffichePraticien : Form
+    {
+        private List<Models.Domain.Praticien> mesPraticiens = null;
+        public FAffichePraticien(List<Models.Domain.Praticien> mesPrat)
+        {
+            InitializeComponent();
+            mesPraticiens = mesPrat;
+        }
+
+        private void btFermer_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FAffichePraticien_Load(object sender, EventArgs e)
+        {
+            dgvPraticiens.
+        }
+
+
+    }
+}
