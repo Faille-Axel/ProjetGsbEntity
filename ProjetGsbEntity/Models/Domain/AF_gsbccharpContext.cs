17775df79ae57fa962fa5a9f9ce16819e02eb1dc
Faille Axel <axelfaille45@gmail.com>
2023-04-06 15:29:18 +0200

    debug the warning message

--- ProjetGsbEntity/Models/Domain/AF_gsbccharpContext.cs
+++ ProjetGsbEntity/Models/Domain/AF_gsbccharpContext.cs
@@ -44,7 +44,6 @@ namespace ProjetGsbEntity.Models.Domain
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseMySql("server=172.30.32.100;port=3306;user=usersio;password=sio;convert zero datetime=True;database=AF_gsbccharp", x => x.ServerVersion("10.4.11-mariadb"));
             }
         }
